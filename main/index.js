ae9e406a2445ada8e1f4e57d3f278674d141431b
yohan <yohan.folliot@gmail.com>
2020-12-19 01:27:40 +0100

    Ajout de la présentation et des liens internes à la page (en scroll onClick)

--- main/index.js
+++ main/index.js
@@ -1,7 +1,7 @@
 
 function hideMenuOnClick() {
 
-    var $footerMenu = document.getElementById("footer-menu");
+    var $footerMenu = document.getElementById("header-menu");
     var isOpen = $footerMenu.classList.contains('slide-in');
     
     $footerMenu.setAttribute('class', isOpen ? 'slide-out' : 'slide-in');
@@ -12,3 +12,13 @@ function hideMenuOnClick() {
         $footerMenu.style.visibility = 'hidden';
     }
 }
+
+function onLinkToPresentation(target) {
+    if (target === "top") {
+        document.getElementById("top").scrollIntoView({behavior: 'smooth'});
+    }
+    else {
+        document.getElementById(target).scrollIntoView({behavior: 'smooth'});
+    }
+    // will scroll to 4th h3 element
+  }
